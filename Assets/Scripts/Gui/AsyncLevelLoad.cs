5faf5df14d7af2fa1f0ae12842fe3b363792b51e
vectorman1 <boris.lachevbg00@gmail.com>
2015-06-22 15:38:47 +0300

    added main menu, splash etc etc.

---
+++ Assets/Scripts/Gui/AsyncLevelLoad.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class AsyncLevelLoad : MonoBehaviour {
+
+    
+	// Use this for initialization
+    IEnumerator Start()
+    {
+        AsyncOperation async = Application.LoadLevelAsync("tutorial-level");
+        yield return async;
+        async = Application.LoadLevelAsync("second-level");
+        yield return async;
+        async = Application.LoadLevelAsync("main-menu");
+        Debug.Log("Loading complete");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Application.GetStreamProgressForLevel("tutorial-level") == 1 &&
+            Application.GetStreamProgressForLevel("second-level") == 1 &&
+            Application.GetStreamProgressForLevel("main-menu") == 1)
+        {
+            Application.LoadLevel("main-menu");
+        }
+        Debug.Log(Application.GetStreamProgressForLevel("tutorial-level"));
+
+	}
+}
---
+++ Assets/Scripts/Gui/MainMenuButtonController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void LoadTutorial()
+    {
+        Application.LoadLevel("tutorial-level");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadLevel_1()
+    {
+        Application.LoadLevel("second-level");
+    }
+}
